222130aa013a68d8632d054ba7a0220da8ba9cbb
Douglas Baltazar <douglasbaltazar1@gmail.com>
2021-04-05 10:34:29 -0300

    Terminado o curso

---
+++ src/components/BookList.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <ul>
+      <book-item
+        v-for="book in books"
+        :key="book.author"
+        :book="book"
+      ></book-item>
+    </ul>
+    <div>
+      <book-form :books="books" @addBook="appendBook"></book-form>
+    </div>
+  </div>
+</template>
+
+<script>
+import BookForm from "./BookForm";
+import BookItem from "./BookItem";
+export default {
+  name: "BookList",
+  components: {
+    BookItem,
+    BookForm,
+  },
+  methods: {
+    appendBook(bookTitle, bookAuthor) {
+      this.books.push({ title: bookTitle, author: bookAuthor });
+    },
+  },
+  data() {
+    return {
+      title: "All Books",
+      books: [
+        { title: "Self-Reliance", author: "Ralph Waldo Emerson" },
+        { title: "American Gods", author: "Neil Gaiman" },
+        { title: "Amusing Ourselves to Death", author: "Neil Postman" },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+</style>
